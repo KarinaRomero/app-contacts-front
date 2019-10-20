a6990b61210cf20aed91373f7e2398db343c1527
Karina Romero <betzy_skamm@hotmail.com>
2019-10-19 22:43:39 -0700

    added model classes

---
+++ src/app/models/contact.ts
@@ -0,0 +1,13 @@
+export class Contact {
+    name: string;
+    age: number;
+    nickName: string;
+    phoneNumber: string;
+
+    constructor(name: string, age: number, nickName: string, phoneNumber: string) {
+        this.name = name;
+        this.age = age;
+        this.nickName = nickName;
+        this.phoneNumber = phoneNumber;
+    }
+}
